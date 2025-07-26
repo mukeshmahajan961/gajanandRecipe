64318aad6758a9bd7f1f9c6cdac18ed8edd0e7dd
Harsh Modi <harsh@glowstardiam.com>
2025-07-26 17:21:32 +0530

    admin panel design add

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+isCollapsed = false;
+
+toggleMenu() {
+  this.isCollapsed = !this.isCollapsed;
+}
+
 
 }
